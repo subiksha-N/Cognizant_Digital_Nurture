7cf122a13ef370849a1d6a29248c23ffa31b561c
subiksha-N <subikshasubiksha74@gmail.com>
2026-05-31 13:44:45 +0530

    Create UsingJavapToInspectBytecode.java

---
+++ CTS-CoreJava/UsingJavapToInspectBytecode.java
@@ -0,0 +1,12 @@
+public class UsingJavapToInspectBytecode {
+
+    public void display() {
+
+        System.out.println("Hello Bytecode");
+    }
+
+    public static void main(String[] args) {
+
+        new UsingJavapToInspectBytecode().display();
+    }
+}
