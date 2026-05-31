71e92109840bba7b5eff595f59021daa0f1d69d4
subiksha-N <subikshasubiksha74@gmail.com>
2026-05-31 13:41:15 +0530

    Create TCPClientServerChat.java

---
+++ CTS-CoreJava/TCPClientServerChat.java
@@ -0,0 +1,25 @@
+import java.io.*;
+import java.net.*;
+
+public class Server {
+
+    public static void main(String[] args)
+            throws Exception {
+
+        ServerSocket server =
+                new ServerSocket(5000);
+
+        System.out.println("Waiting...");
+
+        Socket socket = server.accept();
+
+        BufferedReader br =
+                new BufferedReader(
+                        new InputStreamReader(
+                                socket.getInputStream()));
+
+        System.out.println(br.readLine());
+
+        server.close();
+    }
+}
