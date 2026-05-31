23143db5dd4e055381fcddcce2cd8bea3686a1c2
subiksha-N <subikshasubiksha74@gmail.com>
2026-05-31 13:43:09 +0530

    Update TCPClientServerChat.java

--- CTS-CoreJava/TCPClientServerChat.java
+++ CTS-CoreJava/TCPClientServerChat.java
@@ -23,3 +23,24 @@ public class Server {
         server.close();
     }
 }
+import java.io.*;
+import java.net.*;
+
+public class Client {
+
+    public static void main(String[] args)
+            throws Exception {
+
+        Socket socket =
+                new Socket("localhost",5000);
+
+        PrintWriter pw =
+                new PrintWriter(
+                        socket.getOutputStream(),
+                        true);
+
+        pw.println("Hello Server");
+
+        socket.close();
+    }
+}
