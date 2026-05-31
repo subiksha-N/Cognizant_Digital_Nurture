19d3343a1a9830cbf52dd566ebd488817e8a2834
subiksha-N <subikshasubiksha74@gmail.com>
2026-05-31 10:31:55 +0530

    Create Exercise20_UserEngagementIndex.sql

---
+++ CTS-SQL/Exercise20_UserEngagementIndex.sql
@@ -0,0 +1,9 @@
+SELECT u.full_name,
+       COUNT(DISTINCT r.event_id) AS attended_events,
+       COUNT(DISTINCT f.feedback_id) AS feedbacks
+FROM Users u
+LEFT JOIN Registrations r
+ON u.user_id = r.user_id
+LEFT JOIN Feedback f
+ON u.user_id = f.user_id
+GROUP BY u.full_name;
