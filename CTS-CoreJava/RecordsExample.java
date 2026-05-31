410a8d5966da75612da9fb38e707426eec50d1ae
subiksha-N <subikshasubiksha74@gmail.com>
2026-05-31 13:35:30 +0530

    Create RecordsExample.java

---
+++ CTS-CoreJava/RecordsExample.java
@@ -0,0 +1,13 @@
+record Person(String name, int age) {}
+
+public class RecordsExample {
+
+    public static void main(String[] args) {
+
+        Person p1 = new Person("Dharaneesh", 21);
+        Person p2 = new Person("Karthik", 22);
+
+        System.out.println(p1);
+        System.out.println(p2);
+    }
+}
