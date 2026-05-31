05440516c676b6b6fcade7ebb051194a03407f46
subiksha-N <subikshasubiksha74@gmail.com>
2026-05-31 13:26:56 +0530

    Create ClassAndObjectCreation.java

---
+++ CTS-CoreJava/ClassAndObjectCreation.java
@@ -0,0 +1,26 @@
+class Car {
+
+    String make;
+    String model;
+    int year;
+
+    void displayDetails() {
+        System.out.println("Make: " + make);
+        System.out.println("Model: " + model);
+        System.out.println("Year: " + year);
+    }
+}
+
+public class ClassAndObjectCreation {
+
+    public static void main(String[] args) {
+
+        Car car1 = new Car();
+
+        car1.make = "Toyota";
+        car1.model = "Innova";
+        car1.year = 2024;
+
+        car1.displayDetails();
+    }
+}
