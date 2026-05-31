945e7ba5f9c0a6379bc234f31dfdc1737a515d56
subiksha-N <subikshasubiksha74@gmail.com>
2026-05-31 13:19:42 +0530

    Add DataTypeDemonstration class to showcase types

---
+++ CTS-CoreJava/DataTypeDemonstration.java
@@ -0,0 +1,16 @@
+public class DataTypeDemonstration {
+    public static void main(String[] args) {
+
+        int num = 10;
+        float f = 10.5f;
+        double d = 20.99;
+        char ch = 'A';
+        boolean flag = true;
+
+        System.out.println("Int: " + num);
+        System.out.println("Float: " + f);
+        System.out.println("Double: " + d);
+        System.out.println("Char: " + ch);
+        System.out.println("Boolean: " + flag);
+    }
+}
