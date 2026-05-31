326761cf20c1fdd2f6a049b606b56cd107e5ecf5
subiksha-N <subikshasubiksha74@gmail.com>
2026-05-31 10:34:47 +0530

    Create Exercise24_AverageSessionDurationPerEvent.sql

---
+++ CTS-SQL/Exercise24_AverageSessionDurationPerEvent.sql
@@ -0,0 +1,12 @@
+SELECT e.title,
+       AVG(
+           TIMESTAMPDIFF(
+               MINUTE,
+               s.start_time,
+               s.end_time
+           )
+       ) AS avg_duration
+FROM Events e
+JOIN Sessions s
+ON e.event_id = s.event_id
+GROUP BY e.title;
